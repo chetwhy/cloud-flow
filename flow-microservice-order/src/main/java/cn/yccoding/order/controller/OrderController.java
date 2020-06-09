76fdd1459d2ae317dcf3946d7075e00b72b00745
chetwhy <691409320@qq.com>
2020-06-09 15:59:39 +0800

    修改订单

--- flow-microservice-order/src/main/java/cn/yccoding/order/controller/OrderController.java
+++ flow-microservice-order/src/main/java/cn/yccoding/order/controller/OrderController.java
@@ -1,12 +1,11 @@
 package cn.yccoding.order.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Or;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -21,13 +20,13 @@ import java.util.Map;
 @RequestMapping("/api/v1/orders")
 public class OrderController {
 
-    User user1 = new User(1, "apple", 12, "aaa");
-    User user2 = new User(2, "amazon", 16, "bbb");
-    User user3 = new User(3, "mi", 18, "ccc");
-    User user4 = new User(4, "face", 5, "ddd");
+    Order order1 = new Order(1, "apple", 12, "aaa");
+    Order order2 = new Order(2, "amazon", 16, "bbb");
+    Order order3 = new Order(3, "mi", 18, "ccc");
+    Order order4 = new Order(4, "face", 5, "ddd");
 
     // 默认订单数据集合
-    public List<User> users = Arrays.asList(user1, user2, user3, user4);
+    public List<Order> orders = Arrays.asList(order1, order2, order3, order4);
 
     /**
      * 获取所有订单数据
@@ -35,7 +34,7 @@ public class OrderController {
     @GetMapping("/list")
     public RespResult listUsers() {
         Map<String, Object> data = new HashMap<>();
-        data.put("items", users);
+        data.put("items", orders);
         RespResult result = new RespResult(20000, "请求成功", data);
         return result;
     }
@@ -45,10 +44,29 @@ public class OrderController {
      */
     @GetMapping("/{id}")
     public RespResult findById(@PathVariable Integer id) {
-        User user = this.users.stream().filter(i -> i.getId() == id).findFirst().orElse(null);
+        Order order = this.orders.stream().filter(i -> i.getId() == id).findFirst().orElse(null);
         Map<String, Object> data = new HashMap<>();
-        data.put("item", user);
+        data.put("item", order);
         RespResult result = new RespResult(20000, "请求成功", data);
+        return result;
+    }
+
+    @PutMapping("/{id}")
+    public RespResult updateById(@RequestBody Order order, @PathVariable Integer id) {
+        for (Order o : orders) {
+            if (o.id == id) {
+                BeanUtils.copyProperties(order,o);
+                break;
+            }
+        }
+        RespResult result = new RespResult(20000, "更新成功",null);
+        return result;
+    }
+
+    @PostMapping
+    public RespResult save(@RequestBody Order order) {
+        orders.add(order);
+        RespResult result = new RespResult(20000, "保存成功",null);
         return result;
     }
 
@@ -70,7 +88,7 @@ public class OrderController {
     @Data
     @AllArgsConstructor
     @NoArgsConstructor
-    static class User {
+    static class Order {
         private Integer id;
         private String product;
         private Integer total;
