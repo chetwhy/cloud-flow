40000da57b358d3a796629e8031072bc91e76bca
chetwhy <691409320@qq.com>
2020-11-28 00:59:27 +0800

    feat: 单例模式

---
+++ flow-framework-demo/src/main/java/cn/yccoding/demo/designpattern/singleton/EnumSingleton.java
@@ -0,0 +1,25 @@
+package cn.yccoding.demo.designpattern.singleton;
+
+/**
+ * 单例模式--枚举
+ *
+ * @author YC
+ * @since 2020/11/27
+ */
+public enum EnumSingleton {
+    /**
+     * 天然不支持反射创建对应的实例，且有自己的反序列化机制
+     * 利用类加载机制保证线程安全
+     */
+    ENUM_INSTANCE("singleton","enum"),
+    LAZY_INSTANCE("singleton","lazy");
+
+    private String name;
+    private String type;
+
+    EnumSingleton(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+}
+
